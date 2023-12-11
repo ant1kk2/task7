725acc58aca6cd87714ae52cfce4676f8c066ed5
Anton Konstantynov <batann1990@gmail.com>
2023-12-12 00:03:06 +0200

    final

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ function removeElement(array, item) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === item) {
       array.splice(i, 1);
+      i--
     }
   }
   return array;
 }
 
-const array = [1, "2", 5, 3, "5", 4, 5, 6, "7"];
+const array = [3, 3, 1, "2", 5, 5, 5, 5, 5, 3, 3, 3, 3, "5", 4, 5, 6, "7", 3, 3];
 removeElement(array, 5);
 console.log(array);
